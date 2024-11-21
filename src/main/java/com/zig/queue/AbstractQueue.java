f19cf3546222c054b8bc6e4c12d55090f2781e08
Zagidullin Ayrat <zagidull007@gmail.ru>
2024-11-21 18:16:12 +0300

    Queue - добавил АТД и реализацию Queue, рабор по DynArray

---
+++ src/main/java/com/zig/queue/AbstractQueue.java
@@ -0,0 +1,34 @@
+package com.zig.queue;
+
+public abstract class AbstractQueue<T> {
+    public final static int DEQUEUE_OK = 1;
+    public final static int DEQUEUE_ERR = 2;
+
+    public final static int PEEK_OK = 1;
+    public final static int PEEK_ERR = 2;
+
+    // констуктор
+    // постуловие: создана новая пустая очередь
+    public AbstractQueue() {
+    }
+
+    // команды
+    // постуловие: добавлен элемент item в конец оцереди
+    public abstract void enqueue(T item);
+
+    // пердусловие: очередь не пуста
+    // постуловие: элмент удален из головы очереди
+    public abstract void dequeue();
+
+    // запросы
+    // предусловие: очередь не пуста
+    public abstract T peek();
+
+    // дополнительные запросы
+    // возвращает текущий размер очереди
+    public abstract int size();
+
+    // запросы статусов
+    public abstract int getDequeueStatus(); // успешно; очередь пуста
+    public abstract int getPeekStatus(); // успешно; очередь пуста
+}
